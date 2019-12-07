e724fdf01fb78c7dd07373b55bc4ff0e51015816
Anthony Li <li.anthony917@gmail.com>
2019-12-07 14:36:28 -0500

    Add square

--- PageTest.gd
+++ PageTest.gd
@@ -6,6 +6,16 @@ extends Spatial
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var VR = ARVRServer.find_interface("Oculus")
+	if VR and VR.initialize():
+		get_viewport().arvr = true
+		get_viewport().hdr = false
+		
+		OS.vsync_enabled = false
+		Engine.target_fps = 90
+        # Also, the physics FPS in the project settings is also 90 FPS. This makes the physics
+        # run at the same frame rate as the display, which makes things look smoother in VR!
+	
 	var viewport = get_node("Viewport")
 	viewport.set_clear_mode(Viewport.CLEAR_MODE_ONLY_NEXT_FRAME)
 	
