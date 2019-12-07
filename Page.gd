7c1b26447f671b4873ad4efa73f03fdbedc9be98
Anthony Li <me@anli.dev>
2019-12-07 18:11:45 -0500

    Switch to tabs

--- Page.gd
+++ Page.gd
@@ -3,26 +3,28 @@ extends Node2D
 var query_thread
 
 func _query(input):
-    if input.length() != 0:
-        var url = str("https://en.wikipedia.org/wiki/" + input)
-        print(url)
-        var res = get_viewport().size.x
-        print(res)
-        var output = []
-        OS.execute("node", ["wikipage.js", url, res], true, output)
-        
-        print(output)
-        print(2)
-        
-        var im = Image.new()
-        im.load("output.png")
-        print("a")
-        var texture = ImageTexture.new()
-        print("b")
-        texture.create_from_image(im)
-        print("c")
-        get_node("Sprite").texture = texture
-        print(3)
+	if input.length() != 0:
+		var url = str("https://en.wikipedia.org/wiki/" + input)
+		print(url)
+		var res = get_viewport().size.x
+		print(res)
+		var output = []
+		OS.execute("node", ["wikipage.js", url, res], true, output)
+		
+		print(output)
+		print(2)
+		
+		var im = Image.new()
+		im.load("output.png")
+		print("a")
+		var texture = ImageTexture.new()
+		print("b")
+		texture.create_from_image(im)
+		print("c")
+		get_node("Sprite").texture = texture
+		print(3)
+		
+		
 
 func load_page(name):
 	#self._query(null)
