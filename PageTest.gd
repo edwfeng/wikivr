af155a6e5d7e284e73b52cdeb48e129ec925df2a
Anthony Li <me@anli.dev>
2019-12-07 18:31:14 -0500

    Add link parsing

--- PageTest.gd
+++ PageTest.gd
@@ -1,8 +1,51 @@
 extends Spatial
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var query_thread
+var links
+
+func _query(input):
+	if input.length() != 0:
+		var url = str("https://en.wikipedia.org/wiki/" + input)
+		print(url)
+		var res = get_viewport().size.x
+		print(res)
+		var output = []
+		OS.execute("node", ["wikipage.js", url, res], true, output)
+		
+		print(output)
+		print(2)
+		
+		var im = Image.new()
+		im.load("output.png")
+		print("a")
+		var texture = ImageTexture.new()
+		print("b")
+		texture.create_from_image(im)
+		print("c")
+		get_node("Viewport/Node2D").get_node("Sprite").texture = texture
+		print(3)
+		
+		var file = File.new()
+		
+		print(4)
+		file.open("output.json", File.READ)
+		
+		print(5)
+		links = JSON.parse(file.get_line()).result
+		
+		print(links)
+		
+		refresh_orbs()
+		
+
+func refresh_orbs():
+	pass
+
+func load_page(name):
+	#self._query(null)
+	query_thread = Thread.new()
+	query_thread.start(self, "_query", name)
+	#query_thread.wait_to_finish()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -24,6 +67,8 @@ func _ready():
 
 	var plane = get_node("Plane")
 	(plane.get_surface_material(0)).albedo_texture = viewport.get_texture()
+	
+	load_page("United_States_of_America")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
