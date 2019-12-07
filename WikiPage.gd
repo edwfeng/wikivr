b9a870431ae51a847879f380ad7bb03b6278742a
Edward Feng <feng.dingding@gmail.com>
2019-12-07 17:00:30 -0500

    Fix sizing

--- WikiPage.gd
+++ WikiPage.gd
@@ -9,19 +9,16 @@ func _query(null_arg):
     if input.length() != 0:
         var url = str("https://en.wikipedia.org/wiki/" + input)
         print(url)
-        var res = get_viewport().x
+        var res = get_viewport().size.x
         print(res)
         var output = []
         OS.execute("node", ["wikipage.js", url, res], true, output)
         
-        
         print(output)
         print(2)
         
-        # $Sprite.texture = load("output.png")
         var im = Image.new()
         im.load("output.png")
-        # im.resize(res, im.get_height() * (res/im.get_width()))
         print("a")
         var texture = ImageTexture.new()
         print("b")
@@ -32,7 +29,7 @@ func _query(null_arg):
 
 func _on_Button_pressed():
     if $TextEdit.get_text().length() != 0:
-        self._query(null)
-        #query_thread = Thread.new()
-        #query_thread.start(self, "_query")
+        #self._query(null)
+        query_thread = Thread.new()
+        query_thread.start(self, "_query")
         #query_thread.wait_to_finish()
