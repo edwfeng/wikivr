fb4894ee20d6a4eae6dbedb4a9710b967e006c2d
Anthony Li <me@anli.dev>
2019-12-07 19:34:20 -0500

    final

--- PageTest.gd
+++ PageTest.gd
@@ -33,13 +33,46 @@ func _query(input):
 		print(5)
 		links = JSON.parse(file.get_line()).result
 		
-		print(links)
+		# print(links)
 		
 		refresh_orbs()
 		
 
+var ids = {}
+
 func refresh_orbs():
-	pass
+	print("Refreshing orbs")
+	
+	var o = get_node("O")
+	for n in o.get_children():
+		o.remove_child(n)
+		n.queue_free()
+		
+	ids = {}	
+	
+	for l in range(min(len(links), 100)):
+		var mesh = MeshInstance.new()
+		var plane = SphereMesh.new()
+		mesh.set_mesh(plane)
+		mesh.set_translation(Vector3(l * 3, 7, 2))
+		
+		var material = SpatialMaterial.new()
+		material.albedo_color = Color(1.0, 1.0, 1.0)
+		mesh.set_surface_material(0, material)
+		
+		o.add_child(mesh)
+		
+		var static_body = StaticBody.new()
+		var sp = SphereShape.new()
+		sp.set_radius(1)
+		sp.set_translation(Vector3(l * 3, 7, 2))
+		sp.set_rotation(Vector3(90, 90, 90))
+		
+		static_body.add_child(sp)
+		ids[sp.get_translation()] = links[l].name.replace(" ", "_")
+		o.add_child(static_body)
+		
+		print("Added " + str(mesh.get_translation().x) + " ")
 
 func load_page(name):
 	#self._query(null)
@@ -73,3 +106,9 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
+func thing(target):
+	pass
+	if target.get_children()[0]:
+		if target.get_children()[0].get_translation() in ids:
+			load_page(ids[target.get_children()[0].get_translation()])
